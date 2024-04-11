4fc1641557c32d83cae5dd5bc43a0f724e1f3f09
Charles <charlescon@live.com>
2024-04-10 19:10:55 -0500

    added submit button and a toggle score button

--- src/assets/components/Game/Game.js
+++ src/assets/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useRef, useState} from 'react'
 import styles from './Game.module.scss'
 import Dice from './Dice/Dice';
 
@@ -16,6 +16,7 @@ function Game() {
     const [diceNum, setDiceNum] = useState(6);
     const [turn, setTurn] = useState(0);
     const [minPick, setMinPick] = useState(0);
+    const [hideScore, setHideScore] = useState(true);
     const intervalRef = useRef(null);
 
     function handleRoll() {
@@ -60,36 +61,61 @@ function Game() {
         setDiceNum(6);
     }
 
+    function handleSubmit() {
+
+    }
+
+    function toggleScores() {
+        setHideScore(!hideScore)
+    }
 
     return (
-        <div className={styles.gameCont}>
-            {/* <h1>Play</h1> */}
-            <div className={styles.scorecard}>
-                <h1>Score: {score}</h1>
-            </div>
-            <div className={styles.playerBoard}>
-                {/* {dice.forEach((d,i) => {
-                    return <Dice num={i} value={dice[i].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice}></Dice>
-                })} */}
-                <Dice num={0} value={dice[0].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-                <Dice num={1} value={dice[1].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-                <Dice num={2} value={dice[2].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-                <Dice num={3} value={dice[3].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-                <Dice num={4} value={dice[4].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-                <Dice num={5} value={dice[5].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
-            </div>
+        <div className={styles.board}>
+            {/* <button className={styles.scoreBtn} onClick={() => toggleScores()}>Toggle Scores</button>
+            {hideScore ?  <></>
+                : <div className={styles.scoreList}>
+                    <h3>Scores</h3>
+                    <ul>
+                        <li>score</li>
+                        <li>score</li>
+                        <li>score</li>
+                        <li>score</li>
+                    </ul>
+                </div>} */}
+            <div className={styles.gameCont}>
+                {/* <h1>Play</h1> */}
+                <div className={styles.scorecard}>
+                    <h1>Score: {score}</h1>
+                </div>
+                <div className={styles.playerBoard}>
+                    {/* {dice.forEach((d,i) => {
+                        return <Dice num={i} value={dice[i].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice}></Dice>
+                    })} */}
+                    <Dice num={0} value={dice[0].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                    <Dice num={1} value={dice[1].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                    <Dice num={2} value={dice[2].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                    <Dice num={3} value={dice[3].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                    <Dice num={4} value={dice[4].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                    <Dice num={5} value={dice[5].value} turn={turn} setDiceNum={setDiceNum} diceNum={diceNum} setScore={setScore} score={score} dice={dice} setDice={setDice} setMinPick={setMinPick} minPick={minPick}></Dice>
+                </div>
 
-            <div className={styles.playBtns}>
-                {diceNum === 0 ? <button onClick={() => handleRestart()}>Restart</button> : <button onClick={() => handleRoll()}>Roll</button>}
+                <div className={styles.playBtns}>
+                    {diceNum === 0 
+                        ? <div className={styles.finishBtns}>
+                            <button onClick={() => handleRestart()}>Restart</button>
+                            <button onClick={() => handleSubmit()}>Submit</button>
+                        </div> 
+                        : <button onClick={() => handleRoll()}>Roll</button>}
+                </div>
+                
+                <div className={styles.statsCont}>
+                    <h1>Turn: {turn}</h1>
+                    <h1>Dice Left: {diceNum}</h1>
+                </div>
+                
+                
+                
             </div>
-            
-            <div className={styles.statsCont}>
-                <h1>Turn: {turn}</h1>
-                <h1>Dice Left: {diceNum}</h1>
-            </div>
-            
-            
-            
         </div>
     )
 }
