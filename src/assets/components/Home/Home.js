d8c250096ff8a9aa625bc68234edf5b59de6fe94
Charles <charlescon@live.com>
2024-06-14 15:48:24 -0500

    updated menu spacing

--- src/assets/components/Home/Home.js
+++ src/assets/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Home.module.scss'
 import { useNavigate } from 'react-router-dom'
+import threes from '../../images/number-3.png'
 
 function Home() {
     const navigate = useNavigate();
@@ -11,7 +12,28 @@ function Home() {
 
     return (
         <div className={styles.homeCont}>
+            {/* <div className={styles.homeDiceCont}>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+                <div className={styles.homeDiceBox}>
+                    <h2>3</h2>
+                </div>
+            </div> */}
             <h1>The Game is Threes</h1>
+            {/* <img src={threes} alt='' /> */}
             <h3>Roll 6 dice, lowest score wins</h3>
             <button onClick={() => navigate('/play')}>Play Now</button>
         </div>
