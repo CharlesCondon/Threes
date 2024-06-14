0a6e5ef749139f820edb21b146d0af87f7ddec09
Charles <charlescon@live.com>
2024-06-14 16:46:45 -0500

    added dice to home

--- src/assets/components/Home/Home.js
+++ src/assets/components/Home/Home.js
@@ -1,41 +1,51 @@
 import React from 'react'
 import styles from './Home.module.scss'
 import { useNavigate } from 'react-router-dom'
-import threes from '../../images/number-3.png'
+
+const initialDice = [3,3,3,3,3,3];
 
 function Home() {
+    const [dice, setDice] = React.useState(initialDice);
+    const [styleChange, setStyleChange] = React.useState(false);
     const navigate = useNavigate();
+    const intervalRef = React.useRef(null);
 
     React.useEffect(() => {
         document.title = 'Threes';
     }, []);
 
+    function handleRoll() {
+        clearInterval(intervalRef.current);
+        setStyleChange(true);
+        intervalRef.current = setInterval(() => {
+            const newDice = dice.map((d) => { 
+                d = Math.floor(Math.random()*6+1);
+                return d
+            })
+            setDice(newDice)
+        }, 30);
+
+        setTimeout(() => {
+            clearInterval(intervalRef.current);
+            setDice(initialDice);
+            setStyleChange(false);
+        }, 500);
+    }
+
     return (
         <div className={styles.homeCont}>
-            {/* <div className={styles.homeDiceCont}>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-                <div className={styles.homeDiceBox}>
-                    <h2>3</h2>
-                </div>
-            </div> */}
-            <h1>The Game is Threes</h1>
-            {/* <img src={threes} alt='' /> */}
+            <div className={styles.homeHead}>
+                <h1 className={styles.headTitle}>Threes</h1>
+            </div>
             <h3>Roll 6 dice, lowest score wins</h3>
             <button onClick={() => navigate('/play')}>Play Now</button>
+            <div className={styles.homeDiceCont}>
+                {dice.map((d,i) => {
+                    return <div key={i} onClick={() => handleRoll()} className={`${styles.homeDiceBox} ${styleChange ? styles.activeDiceBox : ''}`}>
+                                <h2>{d}</h2>
+                            </div>
+                })}
+            </div>
         </div>
     )
 }
